4125d8615bacee9564f7e09f4a68486ae84b750e
iamkwan <puttarak555@gmail.com>
2021-06-01 15:42:58 +0700

    Push example code of MASL context/service

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//MSAL Authentication
+import AzureAuthenticationButton from "./azure/azure-authentication-component";
+import { AccountInfo } from "@azure/msal-browser";
+
 function App() {
+  // current authenticated user
+  const [currentUser, setCurrentUser] = useState<AccountInfo>();
+
+  //authentication callback
+  const onAuthenticated = async (userAccountInfo: AccountInfo) => {
+    setCurrentUser(userAccountInfo);
+  };
+
+  //Render JSON data in readable format
+  const PrettyPrintJson = ({ data }: any) => {
+    return (
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    );
+  };
+
+  // Quick link - user revokes app's permission
+  const ShowPermissionRevokeLinks = () => {
+    return (
+      <div>
+        <div><a href="https://myapps.microsoft.com" target="_blank" rel="noopener">Revoke AAD permission</a></div>
+        <div><a href="https://account.live.com/consent/manage" target="_blank" rel="noopener">Revoke Consumer permission</a></div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Microsoft Login/Logout Authentication</h2>
+        <AzureAuthenticationButton onAuthenticated={onAuthenticated} />
+        {
+          currentUser && (
+            <div>
+              <PrettyPrintJson data={currentUser} />
+              <ShowPermissionRevokeLinks />
+            </div>
+          )
+        }
       </header>
     </div>
   );
